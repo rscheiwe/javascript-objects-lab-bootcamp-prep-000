4bc3c964f8a69f77f88c8e5cc0ad94f2ee9e0dac
Richard S <rscheiwe@gmail.com>
2018-03-18 14:29:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ var recipes = {
   'ham' : 'pig'
 };
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  
-  return object
-}
+
 
 function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, object, {[key] : value})
